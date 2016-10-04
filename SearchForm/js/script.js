47cf5fd56c9050ad9d02dbdb0ab67b2161a15933
Roman Bienov <rodergstep@gmail.com>
2016-10-04 23:27:48 +0300

    adding of Classes

--- SearchForm/js/script.js
+++ SearchForm/js/script.js
@@ -23,4 +23,46 @@ function getData(word, quantity) {
     getData(inputValue, 6);
 });
 
-                 
+                 /*CLASSES*/
+
+function Human() {
+    this.name = "Oleksa";
+    this.age = 33;
+    this.male = true;
+    this.height = 200;
+    this.weight = 88;
+}
+
+Worker.prototype = new Human();
+
+function Worker() {
+  this.company = "Granary";
+  this.wage = 3333;
+  this.work = function () {
+    alert(this.name + " works");
+  }
+}
+
+var ornithologist = new Worker();
+ornithologist.age = 22;
+ornithologist.weight = 77;
+ornithologist.wage = 13333;
+
+console.log("Ornithologist's height:", ornithologist.height);
+
+Student.prototype = new Human();
+
+function Student() {
+  this.school = "LNU";
+  this.grants = 888;
+  this.watchMovie = function () {
+    alert(this.name + " watch serials");
+  }
+}
+
+var yasheck = new Student();
+yasheck.grants = 999;
+yasheck.age = 19;
+yasheck.name = "Yasheck";
+
+console.log("Yasheck's weight:", yasheck.weight);
